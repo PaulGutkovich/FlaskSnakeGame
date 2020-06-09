b492d9c22cfca25bd0cb4a054f7febab7b64c2fa
PaulGutkovich <paul.mte5@gmail.com>
2020-06-09 19:21:39 -0400

    a

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, flash
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_required
+from flask_socketio import SocketIO
 from app.auth.routes import auth
 from app.game.routes import game
 from app.config import Config
 from app.models import db, User
+from app.gamehandler import Handler
 
 app = Flask(__name__)
 app.config.from_object(Config)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'auth.login'
+socketio = SocketIO(app)
 
 db.init_app(app)
 
@@ -29,6 +32,16 @@ def test():
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+@socketio.on("connect")
+def connected():
+    #emit('response', {'data': 42})
+    print('we got a connection')
+
+@socketio.on("disconnect")
+def disconnected():
+    #emit('response', {'data': 42})
+    print('we lost a connection')
 
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(game, url_prefix='/game')
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,13 +1,4 @@
-setInterval(ping, 1000);
-
-function ping() {
-    var d = new Date();
-    var t = d.getTime();
-
-    $.get("/game/ping",
-    {"time": t},
-    function(data) {
-        console.log(data);
-    }
-    );
-};
+$(document).ready(function() {
+    var socket = io.connect();
+   window.onunload = socket.disconnect();
+});
