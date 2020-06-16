7f4f0fe2395e23a43ebd058f29e138ee381ecb3a
PaulGutkovich <paul.mte5@gmail.com>
2020-06-16 15:10:22 -0400

    stuff

--- app/gamehandler/__init__.py
+++ app/gamehandler/__init__.py
@@ -1,4 +1,4 @@
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room, emit, send
 from flask_login import current_user
 
 class Handler():
@@ -7,18 +7,27 @@ class Handler():
         self.socketio = SocketIO()
 
         # add socketio event handlers
-        self.socketio.on_event("connect", self.connected, namespace="/game")
-        self.socketio.on_event("add_room", self.add_room, namespace="/game")
+        self.socketio.on_event("connect", self.connected, namespace="/lobby")
+        self.socketio.on_event("add_room", self.add_room, namespace="/lobby")
+        self.socketio.on_event("join", self.on_join, namespace="/lobby")
 
-        # initialize rooms
+        # initialize rooms array
         self.rooms = []
+
+        # initialize players dict
 
     def connected(self):
         print(str(current_user)+" connected to socket")
 
     def add_room(self, data):
         room_name = data["text"]
-        if room_name not in self.rooms:
+        if room_name not in self.rooms and room_name != "":
             self.rooms.append(room_name)
 
-        self.socketio.emit("update_rooms", {"new_rooms": room_name}, namespace="/game")
+            self.socketio.emit("update_rooms", {"new_rooms": self.rooms}, broadcast=True, namespace="/lobby")
+
+    def on_join(self, data):
+        room = data["room"]
+        if room in self.rooms:
+            join_room(room)
+            send("You joined the room "+room)
---
+++ app/static/js/lobby_script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+        var data = {"text": $("#text").val()};
+        socket.emit("add_room", data);
+    });
+});
+
+var socket = io.connect("/lobby");
+var button_clicked = false;
+
+function join_room(room) {
+    if (button_clicked) {
+        button_clicked = false;
+    }
+    else {
+        socket.emit("join", {"room": room});
+        button_clicked = true;
+    }
+};
+
+socket.on("update_rooms", function(data) {
+    var new_rooms = data.new_rooms;
+    var list_html = "";
+    var button_html = "";
+    var target_id = "";
+    var target_button;
+    for (var room of new_rooms) {
+        list_html = list_html.concat("\n".concat("<p>", room, "</p>"));
+        button_html = "\n".concat("<button type='button' id='button_", room, "' onclick='join_room(", "&#39;", room, "&#39;", ")'>Join</button>")
+        list_html = list_html.concat(button_html);
+    }
+
+    $("#room_list").html(list_html);
+
+    for (var room of new_rooms) {
+        target_id = "#button_".concat(room);
+        target_button = $(target_id);
+        target_button.off().on("click", function() {
+            join_room(room);
+        });
+    }
+});
+
+socket.on("message", function(data) {
+    alert(data);
+});
