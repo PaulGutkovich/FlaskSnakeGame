b604cf44fe1614e10ef028ef796f0f689d3ea076
PaulGutkovich <paul.mte5@gmail.com>
2020-09-08 10:23:51 -0400

    minor changes for deployment

--- run.py
+++ run.py
@@ -3,5 +3,4 @@ eventlet.monkey_patch()
 from app import app, handler
 
 if __name__ == '__main__':
-    handler.socketio.run(app, host="192.168.1.154", port=80, debug=True)
-    #handler.socketio.run(app, port=80, debug=True)
+    handler.socketio.run(app, port=80, debug=True)
